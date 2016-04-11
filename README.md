e6859de8bd9015d2d4f5b76424dd1dc8a00fd062
Tesselation <Torsten.Schrade@adwmainz.de>
2016-04-11 08:07:11 +0200

    Added content to repository

---
+++ README.md
@@ -0,0 +1,14 @@
+### 11.04.2016 | Hochschule Mainz
+
+# Nachhaltige Online-Applikationen in den Geisteswissenschaften
+
+## Modellierung &amp; Implementierung
+
+### Torsten Schrade
+
+This repository contains all code, illustrations and images for the
+lecture given at the HS Mainz on 11.04.2016
+
+You can find the presentation online at http://metacontext.github.io/nachhaltige-online-apps
+
+Released under [CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/), Torsten Schrade
---
+++ js/unite.gallery.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function(){
+
+	jQuery("#researchdata").unitegallery({
+		tile_border_color: "#7a7a7a",
+		tile_outline_color: "#8B8B8B",
+		tile_enable_shadow: false,
+		tile_shadow_color: "#8B8B8B",
+		tile_overlay_opacity: 0.5,
+		tile_show_link_icon: true,
+		tile_image_effect_type: "sepia",
+		tile_image_effect_reverse: true,
+		tile_enable_textpanel: true,
+		tile_link_newpage: true,
+		tile_textpanel_always_on: true,
+		tile_textpanel_bg_opacity: 1,
+		tile_textpanel_bg_color:"#464545",
+		lightbox_textpanel_title_color: "e5e5e5",
+		tiles_type: "justified",
+		tiles_col_width: 230,
+		tiles_space_between_cols: 5,
+		tiles_justified_row_height: 170
+	});
+
+});
